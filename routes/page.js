b4bfa15165dd38de5e84f1558d4a9aa1a1be105d
DavidJang <gksrkfkaryghl@hanmail.net>
2020-08-07 00:59:38 +0900

    added function to make to-dolist

--- routes/page.js
+++ routes/page.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var template = require('../lib/template.js');
 var auth = require('../lib/auth');
+const db = require('../lib/db.js');
 
 router.get('/create_ToDoList', function(request, response) {
     var title = "Create To Do List";
@@ -15,17 +16,17 @@ router.get('/create_ToDoList', function(request, response) {
         } else {
             list_num += 1; 
         }
-        
         var added_list = document.getElementById("added_list");
 
         var str = "";
-        str += "<p><input type='text' name='list" + list_num + "' placeholder='오늘의 할일" + list_num + "'></p>";
+        str += "<p><input type='text' name='input' class='list' placeholder='오늘의 할일" + list_num + "'></p>";
 
         var addedDiv = document.createElement("div");
         addedDiv.id = "list_" + list_num;
-        addedDiv.classList.add("list");
         addedDiv.innerHTML = str;
         added_list.appendChild(addedDiv);
+
+        document.getElementById("listCount").innerHTML = "<input type='hidden' name='listCount' value=" + list_num + ">";
     }
 
     function delete_List() {
@@ -38,17 +39,42 @@ router.get('/create_ToDoList', function(request, response) {
             added_list.removeChild(addedDiv);
             list_num -= 1;
             console.log(added_list);
+
+            document.getElementById("listCount").innerHTML = "<input type='hidden' name='listCount' value=" + list_num + ">";
         }
     }
     </script>
-    <form action="/create_ToDoList_process">
-        <p><input type="text" class="list" name="list1" placeholder="오늘의 할일 1">
-        <input type="button" value="항목 추가" onclick="add_List()"> 
-        <input type="button" value="항목 삭제" onclick="delete_List()"><p>
-        <div id="added_list"><div>
+    <form action="/page/create_ToDoList_process" method="post">
+        <div id="new_list">
+            <div></div>
+            <div id="first_list">
+                <input type="text" class="list" name="input" placeholder="오늘의 할일 1">
+                <div id="added_list"></div>
+            </div>
+            <div id="controler">
+                <input type="button" value="항목 추가" onclick="add_List()"> 
+                <input type="button" value="항목 삭제" onclick="delete_List()">
+            </div>
+            <div id="listCount"><input type="hidden" name="listCount" value=1></div>
+        </div>
+        <p><br><input type="submit" value="완료"></p>
     </form>
     `, statusUI);
     response.send(html);
+});
+
+
+router.post("/create_ToDoList_process", function(request, response) {
+    var post = request.body;
+    var listCount = post.listCount;
+    var list = post.input;
+    console.log("list: ", list, "post:", post, post.listCount, post.input[0])
+    
+    for(var i = 0 ; i < listCount; i++) {
+        db.get('toDoList').push(list[i]).write();
+    }
+
+    response.redirect('/');
 });
 
 module.exports = router;
--- routes/toDoList.js
+++ routes/toDoList.js
@@ -2,20 +2,29 @@ const express = require('express');
 const router = express.Router();
 var template = require('../lib/template.js');
 var auth = require('../lib/auth');
+const db = require('../lib/db.js');
 
 router.get('/', function(request, response) {
     var title = "To Do List";
     var statusUI = auth.statusUI(request, response);
-
-    var html = template.HTML(title, 
-    `<p>등록된 할 일이 없습니다.</p>
-    <input type="button"  value="오늘의 할일 설정하기" onclick="createToDoList()">
-    <script type="text/javascript">
-        function createToDoList() {
-            location.href = "/page/create_ToDoList";
+    var description = '';
+    var list = db.get('toDoList').value();
+    if(1 <= list.length) {
+        for(var i = 0 ; i < list.length ; i++) {
+            description = description + `<div class="todayToDoList"><p><input type="checkbox">${list[i]}</p></div>`
         }
-    </script>
-    `, statusUI);
+        console.log(list);
+    } else {
+        description = `<p>등록된 할 일이 없습니다.</p>
+        <input type="button"  value="오늘의 할일 설정하기" onclick="createToDoList()">
+        <script type="text/javascript">
+            function createToDoList() {
+                location.href = "/page/create_ToDoList";
+            }
+        </script>`
+    }
+
+    var html = template.HTML(title, description, statusUI);
     response.send(html);
 });
 
